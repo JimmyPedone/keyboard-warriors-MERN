045ac249cdb32a4fb42dc32b6d05222b836258b2
JimmyPedone <>
2023-08-22 21:16:38 -0400

    added dotenv

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    port: 4444,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:4445',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
 })
